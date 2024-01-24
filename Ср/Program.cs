46a8c980c787e2b694062b4757c47bba9cfb6af0
212206 <212206@1220-010-22>
2024-01-24 11:01:51 +0300

    Добавьте файлы проекта.

---
+++ Ср/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите последовательность скобок:");
+string sequence = Console.ReadLine();
+
+string bracketsOnly = new string(sequence.Where(c => c == '(' || c == ')').ToArray());
+string expressionOnly = new string(sequence.Where(c => c != '(' && c != ')').ToArray());
+
+Console.WriteLine("Скобки: " + bracketsOnly);
+Console.WriteLine("Выражение: " + sequence);
+Console.WriteLine("Результат проверки: " + SequenceOfParentheses(sequence));
+
+static bool SequenceOfParentheses(string input)
+{
+    Stack<char> stack = new Stack<char>();
+
+    foreach (char bracket in input)
+    {
+        if (bracket == '(')
+        {
+            stack.Push(bracket);
+        }
+        else if (bracket == ')')
+        {
+            if (stack.Count == 0 || stack.Peek() != '(')
+            {
+                return false;
+            }
+            stack.Pop();
+        }
+
+    }
+
+    return stack.Count == 0;
+}
